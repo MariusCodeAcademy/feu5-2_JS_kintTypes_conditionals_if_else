80ac7288f588b565453d11419a7b3cd7913b8a58
Marius CA <marius@example.com>
2023-02-07 10:55:29 +0200

    uzd, else if

---
+++ ifElse.js
@@ -0,0 +1,31 @@
+'use strict';
+console.log('if else js loaded');
+
+/* 
+if (condition) {
+  //  block of code to be executed if the condition is true
+}
+*/
+
+let amount = 10;
+
+if (amount < 19) {
+  // vygdoma jei if salyga === true
+  console.log('keletas');
+}
+
+/* if else sintakse
+
+if (condition) {
+  //  block of code to be executed if the condition is true
+} else {
+  //  block of code to be executed if the condition is false
+}
+
+*/
+if (amount < 19) {
+  // vygdoma jei if salyga === true
+  console.log('keletas');
+} else {
+  console.log('daug');
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+## uzd1
+
+1. sukurti nauja faila uzd.js
+2. prijungti prie index.html
+3. sukurti kintamaji year ir prilyginti kazkuriems metams
+4. padaryti kad priklausomai nuo metu programa parasytu "po 2000" arba 'pries 2000'
+   pvz 1900 => pries 2000, jei 2020 => "po 2000".
+   4.1 isbandyti su keletu skaiciu
