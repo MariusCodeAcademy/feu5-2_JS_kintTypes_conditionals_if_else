616a0b46fc1b682d2cc46232d46e757978adfb04
Marius CA <marius@example.com>
2023-02-07 10:41:25 +0200

    palyginimo operatoriai

---
+++ palyginimoOperatoriai.js
@@ -0,0 +1,16 @@
+'use strict';
+console.log('palyginimo operatoriai');
+
+let n1 = 10;
+let n2 = 9;
+let n3 = 0;
+
+// ar n1 yra daugiau uz n2
+console.log(`ar ${n1} yra daugiau uz ${n2} ===> ${n1 > n2}`);
+console.log(`ar ${n1} yra maziau uz ${n2} ===> ${n1 < n2}`);
+console.log(`ar ${n1} yra maziau arba lygu ${n2} ===> ${n1 <= n2}`);
+// === - tikrina ir verte ir tipa
+console.log(`ar ${n1} yra lygu ${n2} ===> ${n1 === n2}`);
+// ! - ne
+console.log(`ar ${n1} yra nelygu ${n2} ===> ${n1 !== n2}`);
+console.log(`ar yra reikme n3 ${n3 !== 0}`);
